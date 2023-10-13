c7819a1bbf51fae8d51fd40bf4bed47fa88e9e9f
ryedbadr1 <ryedbadr1@gmail.com>
2023-10-13 13:46:09 -0500

    fixed file structure and linked login page to python

--- src/python/main.py
+++ src/python/main.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+
+temp = os.path.abspath(os.path.dirname(__file__))
+template_dir = os.path.dirname(os.path.dirname(temp))
+template_dir = os.path.join(template_dir, 'src')
+template_dir = os.path.join(template_dir, 'pages')
+template_dir = os.path.join(template_dir, 'templates')
+static_dir = os.path.dirname(os.path.dirname(temp))
+static_dir = os.path.join(static_dir, 'src')
+static_dir = os.path.join(static_dir, 'pages')
+static_dir = os.path.join(static_dir, 'static')
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 
 @app.route('/')
-def placehold():
-    return 'placeholder'
+def login():
+    return render_template("login.html")
+
+
+if __name__ == '__main__':
+    app.run()
